42d9256de141444585b4020bca13de37574a33ed
Mudit Ameta <zeusdeux@gmail.com>
2020-10-20 15:47:31 +0200

    Switch to a lang::Error struct instead of enum for more flexibility

--- src/lang.rs
+++ src/lang.rs
@@ -1,15 +1,45 @@
 pub mod tokenizer;
-use std::{fmt, result};
+use std::{fmt, iter, result};
 
 #[derive(Debug)]
-pub enum Error {
-    Tokenizer(String),
+enum ErrorKind {
+    Tokenizer,
+    // Parser,
+    // Interpreter
+}
+
+#[derive(Debug)]
+pub struct Error {
+    kind: ErrorKind,
+    line: usize,
+    col: usize,
+    message: String,
+}
+
+impl Error {
+    fn new(kind: ErrorKind, line: usize, col: usize, message: String) -> Self {
+        Error {
+            kind,
+            line,
+            col,
+            message,
+        }
+    }
 }
 
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            Error::Tokenizer(err) => write!(f, "Tokenizer error\n{}", err),
+        match self.kind {
+            ErrorKind::Tokenizer => write!(
+                f,
+                "Tokenizer error on line: {}, column: {}\n{}\n{}{}",
+                self.line,
+                self.col,
+                // remove any extraneous newlines in the error message that prevent alignment of ----^ underneath the errored line
+                self.message.trim(),
+                iter::repeat('-').take(self.col - 1).collect::<String>(),
+                '^'
+            ),
         }
     }
 }
